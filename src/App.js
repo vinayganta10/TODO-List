654169ebd44e383cdbef773b718b055516df9716
vinayganta <aggvinayganta10@gmail.com>
2023-09-03 12:05:44 +0530

    filter button functionality added

--- src/App.js
+++ src/App.js
@@ -9,6 +9,15 @@ function App(props) {
   let[tasks,setTasks]=useState(props.tasks);
   const [filter, setFilter] = useState("All");
 
+  //filter object
+  const FILTER_MAP = {
+    All: () => true,
+    Active: (task) => !task.completed,
+    Completed: (task) => task.completed,
+  };
+  
+  const FILTER_NAMES = Object.keys(FILTER_MAP);
+
   //adding Task function
   function addTask(name){
     const newTask={id:`todo-${nanoid()}`,name,completed:false};
@@ -43,28 +52,42 @@ function App(props) {
     setTasks(editedTaskList);
   }
 
-  const taskList = tasks.map((task) => (
-  <Todo
-    id={task.id}
-    name={task.name}
-    completed={task.completed}
-    key={task.id}
-    toggleTaskCompleted={toggleTaskCompleted}
-    deleteTask={deleteTask}
-    editTask={editTask}
-  />
-));
+  const taskList = tasks
+  .filter(FILTER_MAP[filter])
+  .map((task) => (
+    <Todo
+      id={task.id}
+      name={task.name}
+      completed={task.completed}
+      key={task.id}
+      toggleTaskCompleted={toggleTaskCompleted}
+      deleteTask={deleteTask}
+      editTask={editTask}
+    />
+  ));
 
-  const buttonList = props.button.map((button)=><FilterButton key={button.id} name={button.name} pressed={button.pressed}/>);
+  
+  const filterList = FILTER_NAMES.map((name) => (
+    <FilterButton
+      key={name}
+      name={name}
+      isPressed={name === filter}
+      setFilter={setFilter}
+    />
+  ));
+  
+
+  //const buttonList = props.button.map((button)=><FilterButton key={button.id} name={button.name} pressed={button.pressed}/>);
+  
   let taskNoun = taskList.length===1?"task":"tasks";
   let listHeading = `${taskList.length} ${taskNoun} remaining`;
-  console.log(tasks);
+  
   return (
     <div className="todoapp stack-large">
       <h1>TodoMatic</h1>
       <Form addTask={addTask}/>
       <div className="filters btn-group stack-exception">
-        {buttonList}
+        {filterList}
       </div>
       <h2 id="list-heading">{listHeading}</h2>
       <ul
--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function FilterButton(props){
     return(
-        <button type="button" className="btn toggle-btn" aria-pressed={props.pressed}>
+        <button 
+            type="button" 
+            className="btn toggle-btn" 
+            aria-pressed={props.pressed} 
+            onClick={() => props.setFilter(props.name)} 
+        >
           <span className="visually-hidden">Show </span>
           <span>{props.name}</span>
           <span className="visually-hidden"> tasks</span>
