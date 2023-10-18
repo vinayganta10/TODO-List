c655c74a60ca167f25a6c4690942668dc7f29d82
vinayganta <aggvinayganta10@gmail.com>
2023-10-18 17:49:20 +0530

    Bug fixed

--- src/components/FilterButton.js
+++ src/components/FilterButton.js
@@ -5,7 +5,7 @@ function FilterButton(props){
         <button 
             type="button" 
             className="btn toggle-btn" 
-            aria-pressed={props.pressed} 
+            aria-pressed={props.isPressed} 
             onClick={() => props.setFilter(props.name)} 
         >
           <span className="visually-hidden">Show </span>
